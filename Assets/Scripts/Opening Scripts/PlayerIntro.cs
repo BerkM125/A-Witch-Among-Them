5e5c36b71d5cb40985165a58112f526408241d1f
BerkM125 <49427416+BerkM125@users.noreply.github.com>
2025-05-02 20:51:28 -0700

    programming opening sequence

---
+++ Assets/Scripts/Opening Scripts/PlayerIntro.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerIntro : MonoBehaviour
+{
+    public float speed = 0.2f; // Slow speed for downward movement
+    private Rigidbody2D rb;
+    private float moveDuration = 1f; // Duration of movement in seconds
+    private float elapsedTime = 0f;
+    public DialogueBoxController dialogueBoxController;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0f; // Ensure no gravity affects the player
+        StartCoroutine(MoveDown()); // Start the downward movement coroutine
+        StartCoroutine(OpeningSpeech()); // Start the opening speech coroutine
+    }
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator OpeningSpeech()
+    {
+        dialogueBoxController.ShowDialogue("character_player", "Hear me, people of New Athens! I am a witch hunter. My job is to expose witches in this time of great suffering and cure towns from suffering");
+        yield return new WaitForSeconds(2f); // Wait for 2 seconds before continuing
+    }
+    IEnumerator MoveDown()
+    {
+        // Move downwards for a set duration
+        float startTime = Time.time;
+        while (Time.time < startTime + moveDuration)
+        {
+            rb.MovePosition(rb.position + Vector2.down * speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+}
